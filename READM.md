553ea8c553bb68d494bd5eee1426c40362f3be6b
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-09-12 12:55:30 +0530

    Integration

---
+++ READM.md
@@ -0,0 +1,18 @@
+# Want to integrate Stripe-API in your Web app
+
+Then here's a quick starter for you 
+
+## Installation
+
+Go to Frontend folder and Backend folder and do
+
+```bash
+npm install
+```
+
+## Usage
+
+Just put your Stripe Secret key in .env file in Backend folder and Publishable key in .env file in Frontend folder
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ backend/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config()
+const stripe = require("stripe")(process.env.SECRET_KEY);
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello from simple server :)");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from simple server :)");
+});
+
+app.post("/payment", async (req, res) => {
+  console.log(req.headers.origin);
+
+  try {
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          amount: 100,
+          name: "Shopping",
+          currency: "usd",
+          quantity: 1,
+        },
+      ],
+      payment_method_types: ["card"],
+      // mode: "payment",
+      success_url: `http://localhost:3001/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `http://localhost:3001/?canceled=true`,
+    });
+    // console.log(session);
+    res.redirect(303, session.url);
+  } catch (err) {
+    res.status(err.statusCode || 500).json(err.message);
+  }
+
+  // const idempontencykey = uuidv4();
+
+  // return stripe.customers
+  //   .create({
+  //     email: token.email,
+  //     source: token.id,
+  //   })
+  //   .then((customer) => {
+  //     return stripe.charges.create(
+  //       {
+  //         amount: product.price * 100,
+  //         currency: "usd",
+  //         customer: customer.id,
+  //         receipt_email: token.email,
+  //         description: product.name,
+  //         shipping: {
+  //           name: token.card.name,
+  //           address: {
+  //             country: token.card.address_country,
+  //           },
+  //         },
+  //       },
+  //       { idempontencykey }
+  //     );
+  //   })
+  //   .then((response) => res.status(200).json(response))
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+});
+
+app.listen(5000, () => {
+  console.log("Connected");
+});
